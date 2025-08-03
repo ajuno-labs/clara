a271741daec0ae27a716bc08b16e3ffc80e780e5
Giao Le <giolynx104@gmail.com>
2025-08-03 09:45:43 +0700

    Create task struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use chrono::{DateTime, Utc};
+use std::fmt;
+
+
+#[derive(Debug)]
+enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
+
+struct Task {
+    id: u32,
+    title: String,
+    status: Status,
+    created_at: DateTime<Utc>,
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Task ID: {}, Title: {}, Status: {:?}, Created At: {}",
+            self.id, self.title, self.status, self.created_at
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task = Task {
+        id: 1,
+        title: String::from("Write clara repl"),
+        created_at: Utc::now(),
+        status: Status::Todo,
+    };
+
+    println!("Task created: {}", task);
 }
